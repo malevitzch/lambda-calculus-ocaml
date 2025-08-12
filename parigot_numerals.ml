24f1a5ccfb8d533b62791ef83d78a4717a6ec739
malevitzch <malevitzch@gmail.com>
2025-08-12 11:03:06 +0200

    It turns out I miswrote the definition and accidentally got church numerals instead

--- parigot_numerals.ml
+++ parigot_numerals.ml
@@ -3,6 +3,11 @@ let successor n x f = f (n x f)
 let rec parigot n x f =
   if n = 0
     then x
-    else successor (parigot (n - 1)) x f
+    else f ((parigot (n - 1)) x f)
 
 let eval n = n 0 (fun x -> x + 1)
+
+
+let pred n x f =
+  let g = fun p -> (true, if fst p then f x else x) in
+    snd (n (false, x) g)
