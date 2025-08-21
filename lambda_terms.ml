03cd349f0973191412a467d16aa6d213810441e5
malevitzch <malevitzch@gmail.com>
2025-08-21 13:31:30 +0200

    Left-associativity of application is now considered when printing parentheses

--- lambda_terms.ml
+++ lambda_terms.ml
@@ -13,7 +13,8 @@ module Lambda = struct
         | App (t1, t2) ->
           (match t1 with 
             | Var y -> y
-            | _ -> "(" ^ (term_to_str t1) ^ ")")
+            | App _ -> term_to_str t1
+            | Abs _ -> "(" ^ (term_to_str t1) ^ ")")
           ^
           (match t2 with
             | Var y -> y
