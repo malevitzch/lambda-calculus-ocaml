d4710630d14bc1acb92515d780e6d81652fd48cb
malevitzch <malevitzch@gmail.com>
2025-08-23 10:56:02 +0200

    Added explanations to the functions used

--- lambda_terms.ml
+++ lambda_terms.ml
@@ -2,24 +2,33 @@ module Lambda = struct
 
   let lambda = "λ"
 
+  (*
+    A term is either a variable, an application of one term to another,
+    or an abstraction in the form of λx.M, which basically reads:
+    "for any x give M", where M might contain x inside itself
+  *)
   type term =
     | Var of string
     | App of term * term
     | Abs of string * term
 
-    let rec term_to_str (t: term) : string =
-      match t with
-        | Var x -> x
-        | App (t1, t2) ->
-          (match t1 with 
-            | Var y -> y
-            | App _ -> term_to_str t1
-            | Abs _ -> "(" ^ (term_to_str t1) ^ ")")
-          ^
-          (match t2 with
-            | Var y -> y
-            | _ -> "(" ^ (term_to_str t2) ^ ")")
-        | Abs (x, t1) -> lambda ^ x ^ "." ^ (term_to_str t1)
+  (*
+    Conversion of lambda terms to strings
+    Avoids printing unnecessary parentheses
+  *)
+  let rec term_to_str (t: term) : string =
+    match t with
+      | Var x -> x
+      | App (t1, t2) ->
+        (match t1 with 
+          | Var y -> y
+          | App _ -> term_to_str t1
+          | Abs _ -> "(" ^ (term_to_str t1) ^ ")")
+        ^
+        (match t2 with
+          | Var y -> y
+          | _ -> "(" ^ (term_to_str t2) ^ ")")
+      | Abs (x, t1) -> lambda ^ x ^ "." ^ (term_to_str t1)
 
   type 'a maybe_changed = Changed of 'a | Unchanged of 'a
 
@@ -27,22 +36,63 @@ module Lambda = struct
     match mc with
       | Unchanged c | Changed c -> c
 
-  let rec substitute (target: term) (var: string) (t: term) : term = 
+  (*
+    Function for performing lambda calculus substitutions.
+    We denote subsitutions as M[x := P], which is read as:
+    replace every "free" x in M with P. A free variable is one that is not bound
+    by an abstraction.
+
+    The arguments are (in order):
+      - The name of the variable you want to substitute for
+      - The term that you substitute for said variable
+      - The term you want to perform the substitution on
+  *)
+  let rec substitute (var: string) (target: term) (t: term) : term = 
     match t with 
       | Var x -> if x = var then target else t
-      | App (t1, t2) -> App (substitute target var t1, substitute target var t2)
-      | Abs (x, t1) -> if x = var then t else Abs (x, substitute target var t1)
+      | App (t1, t2) -> App (substitute var target t1, substitute var target t2)
+      | Abs (x, t1) -> if x = var then t else Abs (x, substitute var target t1)
 
+  (*
+    Auxiliary function to check whether a term is a beta-redex,
+    basically a term of form (λx.M)P which, by beta-reduction,
+    we treat as nothing else than function application, thus
+    (λx.M)P -> M[x := P] (substitute x in M for P).
+  *)
   let is_beta_redex (t: term) : bool = 
     match t with
       | App (Abs (_, _), _) -> true
       | _ -> false
 
-  let rec standard_reduction (t: term) : term maybe_changed = 
+  (*
+    This function performs a single step of the normalizing
+    leftmost reduction strategy on term t and returns the result
+    along with the information whether any redex has been reduced.
+
+    As a result of the standariztion theorem, repeatedly applying
+    this function until no changes are made yields 
+    the normal form of the term t (if one exists).
+
+    But beware: the problem of finding whether an untyped lambda calculus term
+    has a normal form is undecideable (belongs to RE but not to CORE).
+
+    Therefore repeatedly applying this function is not always a good idea
+    since the term might not have a normal form and the program would run forever.
+    It's suggested to put an upper limit on the number of reductions you allow.
+
+    And no, there is no "universal" limit that you can calculate based on
+    size of the term. This is closely related to the undecideable problem of
+    whether a program is a "busy beaver champion".
+    For more information, look here: https://en.wikipedia.org/wiki/Busy_beaver
+
+    This function takes a single argument: the term t that you want to perform
+    the reduction on.
+  *)
+  let rec standard_reduction (t: term) : term maybe_changed =
     match t with
     | Var _ -> Unchanged t
-    | App (Abs (x, t1), t2) -> Changed (substitute t2 x t1)
-    | Abs (x, t1) -> 
+    | App (Abs (x, t1), t2) -> Changed (substitute x t2 t1) (* beta-reduction *)
+    | Abs (x, t1) ->
       (match (standard_reduction t1) with 
       | Unchanged t2 -> Unchanged (Abs (x, t2))
       | Changed t2 -> Changed (Abs (x, t2)))
@@ -58,6 +108,6 @@ module Lambda = struct
 
 
 let identity : term = Abs ("x", Var "x")
-let omega : term = Abs ("x", App (Var "f", App (Var "x", Var "x")))
+let omega : term = Abs ("x", App (Var "x", Var "x"))
 let bigomega : term = App (omega, omega)
 end
